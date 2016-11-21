79884de7a9d12b99760641d6539203fe1ef1f88e
Kwan Holloway <kwanholloway@newdesignhigh.com>
2016-11-21 00:56:41 -0500

    Testing JS

--- js/test_script1.js
+++ js/test_script1.js
@@ -107,17 +107,20 @@ $(document).ready(function () { // load on opening of html page, JQUERY START
       }
       //fade out About Section
       $('.AboutSection').css({
-        'opacity': ( ( height - (scrollTop - abtHgt)  ) / height*2 )
+//        'opacity': ( ( height - (scrollTop - abtHgt)  ) / height )
+        'opacity': ((height - scrollTop) / height)
       });
       
       // fade out Artwork Section
       $('.ArtworkSection').css({             //height of abtHgt + artHgt
-        'opacity': ( ( height - (scrollTop - (abtHgt + artHgt) )  ) / height*4 )
+//        'opacity': ( ( height - (scrollTop - (abtHgt + artHgt) )  ) / height )
+        'opacity': ((height - scrollTop) / height)
       }); 
 
       // fade out Projects Section
       $('.ProjectsSection').css({             //height of abtHgt + artHgt
-        'opacity': ( ( height - (scrollTop - (abtHgt + artHgt + projHgt) )  ) / height*6 )
+//        'opacity': ( ( height - (scrollTop - (abtHgt + artHgt + projHgt) )  ) / height) )
+        'opacity': ((height - scrollTop) / height)
       }); 
 
 //      $(".icon-menu").click(function() {
