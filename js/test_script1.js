afd90b618e935c278ce9b6d1950459cf7570c0cd
Kwan Holloway <kwanholloway@newdesignhigh.com>
2016-11-21 00:28:25 -0500

    Testing JS

--- js/test_script1.js
+++ js/test_script1.js
@@ -115,10 +115,10 @@ $(document).ready(function () { // load on opening of html page, JQUERY START
         'opacity': ( ( height - (scrollTop - (abtHgt + artHgt) )  ) / (artHgt/7) )
       }); 
 
-      // fade out Projects Section
-      $('.ProjectsSection').css({             //height of abtHgt + artHgt
-        'opacity': ( ( height - (scrollTop - (abtHgt + artHgt + projHgt) )  ) / (projHgt/10) )
-      }); 
+//      // fade out Projects Section
+//      $('.ProjectsSection').css({             //height of abtHgt + artHgt
+//        'opacity': ( ( height - (scrollTop - (abtHgt + artHgt + projHgt) )  ) / (projHgt/10) )
+//      }); 
 //
 //      $(".icon-menu").click(function() {
 //          $(".menu").fadeTo("fast",1);
