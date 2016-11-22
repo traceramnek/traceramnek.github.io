46ad3de144cbb7eb77fe3c59323c2f2f452ed2d8
Kwan Holloway <kwanholloway@newdesignhigh.com>
2016-11-21 20:07:48 -0500

    More JS Debugging

--- js/test_script1.js
+++ js/test_script1.js
@@ -107,7 +107,7 @@ $(document).ready(function () { // load on opening of html page, JQUERY START
       }
       //fade out About Section
       $('.AboutSection').css({
-        'opacity': ( ( height - (scrollTop - abtHgt)  ) / (abtHgt/5) )
+        'opacity': ( ( height - (scrollTop - abtHgt)  ) / (abtHgt/7) )
       });
 //      
 //      // fade out Artwork Section
