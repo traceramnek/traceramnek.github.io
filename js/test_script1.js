e63de0ef9d6e1f9bf1d4c269676c09a165b91c14
Kwan Holloway <kwanholloway@newdesignhigh.com>
2016-11-21 22:15:12 -0500

    More JS Trials

--- js/test_script1.js
+++ js/test_script1.js
@@ -93,7 +93,7 @@ $(document).ready(function () { // load on opening of html page, JQUERY START
 
       //fade out Jumbotron
       $('.jumbotron').css({
-        'opacity': ( ( 1 - scrollTop  ) / ( jumbHgt ) )
+        'opacity': ( ( height - scrollTop  ) / ( 3000 ) )
       }); 
 
       scrollTop = $(this).scrollTop();
