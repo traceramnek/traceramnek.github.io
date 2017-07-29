4d07f840fc1d067f4e7d99b5b0a5478190cc0120
Kwan Holloway <kwanholloway@newdesignhigh.com>
2017-07-29 13:05:37 -0400

    Updates

--- js/test_script1.js
+++ js/test_script1.js
@@ -95,15 +95,15 @@ $(document).ready(function () { // load on opening of html page, JQUERY START
             $('.ToTopButton').css('opacity', 0);
       }
       
-       //Show or hide the To Top Button Arrow
-      if (scrollTop > jumbHgt+10 ) { //show button
-			//$('.ToTopButton').fadeTo('fast',1)
-          $('#header').css('opacity', 1);
-      } 
-      else { // hide button
-			//$('.ToTopButton').fadeTo('fast',0);
-            $('#header').css('opacity', 0);
-      }
+//       //Show or hide the To Top Button Arrow
+//      if (scrollTop > jumbHgt+10 ) { //show button
+//			//$('.ToTopButton').fadeTo('fast',1)
+//          $('#header').css('opacity', 1);
+//      } 
+//      else { // hide button
+//			//$('.ToTopButton').fadeTo('fast',0);
+//            $('#header').css('opacity', 0);
+//      }
       
       //NOTE: (ScrollTop - sectionHgt) gets the size of the div
 
