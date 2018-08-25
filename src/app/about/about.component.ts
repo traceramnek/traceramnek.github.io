58c05ee74c145979ccec29572e3dd57990d386d7
Kwan Holloway <kwansholloway@gmail.com>
2018-08-25 01:26:22 -0400

    Animations on sections of About

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,51 @@
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.less']
+  styleUrls: ['./about.component.less'],
+  animations: [
+    trigger('showSection', [
+      state('inactive', style({
+        height: '0',
+        opacity: '0',
+        padding: '0 15%'
+      })),
+      state('active', style({
+        height: '*',
+        opacity: '1',
+        padding: '*'
+      })),
+      transition('inactive => active', animate('500ms ease-in')),
+      transition('active => inactive', animate('500ms ease-out'))
+    ])
+  ]
 })
 export class AboutComponent implements OnInit {
+  navigationSubState: { [menu: string]: string } = {
+    interests: 'inactive',
+    motivations: 'inactive',
+    facts: 'inactive'
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  toggleNavigationSub(menuName: string, event: Event) {
+    event.preventDefault();
+    this.navigationSubState.interests = 'inactive';
+    this.navigationSubState.motivations = 'inactive';
+    this.navigationSubState.facts = 'inactive';
+
+    this.navigationSubState[menuName] = (this.navigationSubState[menuName] === 'inactive' ? 'active' : 'inactive');
+
+    if (this.navigationSubState[menuName] == 'active') {
+      window.scrollBy(0, 500);
+    }
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 import { NavbarModule } from './navbar/navbar.module';
@@ -25,6 +25,7 @@ import { SharedService } from './services/shared.service';
     ProjectsModule,
     ArtworkModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [
